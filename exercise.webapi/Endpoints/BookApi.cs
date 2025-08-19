5b00b449a3e278665abf128bd836b14398be1ee5
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-19 15:02:51 +0200

    added endpoint for getting a book by its id

--- exercise.webapi/Endpoints/BookApi.cs
+++ exercise.webapi/Endpoints/BookApi.cs
@@ -2,7 +2,6 @@
 using exercise.webapi.Models;
 using exercise.webapi.Repository;
 using Microsoft.AspNetCore.Mvc;
-using static System.Reflection.Metadata.BlobBuilder;
 
 namespace exercise.webapi.Endpoints
 {
@@ -13,6 +12,7 @@ namespace exercise.webapi.Endpoints
             var books = app.MapGroup("books");
 
             books.MapGet("/", GetBooks);
+            books.MapGet("/{id}", GetBookById);
         }
 
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -27,6 +27,21 @@ namespace exercise.webapi.Endpoints
             });
 
             return TypedResults.Ok(bookDtos);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public static async Task<IResult> GetBookById(int id, IBookRepository bookRepository)
+        {
+            var targetBook = await bookRepository.GetBookById(id);
+            if (targetBook == null) return TypedResults.NotFound($"Book with ID {id} not found.");
+            var bookDto = new BookDto
+            {
+                Id = targetBook.Id,
+                Title = targetBook.Title,
+                Author = targetBook.Author == null ? null : new Author { Id = targetBook.Author.Id, FirstName = targetBook.Author.FirstName, LastName = targetBook.Author.LastName, Email = targetBook.Author.Email }  
+            };
+            return TypedResults.Ok(bookDto);
         }
     }
 }
--- exercise.webapi/Repository/BookRepository.cs
+++ exercise.webapi/Repository/BookRepository.cs
@@ -15,8 +15,15 @@ namespace exercise.webapi.Repository
 
         public async Task<IEnumerable<Book>> GetAllBooks()
         {
-            return await _db.Books.Include(b => b.Author).ToListAsync(); 
+            return await _db.Books.Include(b => b.Author).ToListAsync(); // makes sure author data is included in each book
 
+        }
+
+        public async Task<Book> GetBookById(int id)
+        {
+            return await _db.Books
+                .Include(b => b.Author) // include author data
+                .FirstOrDefaultAsync(b => b.Id == id); // find the target book by id
         }
     }
 }
